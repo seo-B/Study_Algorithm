f52ec0d1d796ec7fbff04476e068a638b122d23d
seo-B <latte47ss@gmail.com>
2021-11-08 23:56:25 +0900

    .

--- BaekJoon/4단계 while문/1110-더하기 사이클/app.js
+++ BaekJoon/4단계 while문/1110-더하기 사이클/app.js
@@ -81,3 +81,13 @@ for (let i = 0; ; i++) {
 }
 
 console.log(cycle);
+
+// let count = 0;
+
+// while(true){
+//     let sum = parseInt((parseInt(input/10)) + (input % 10) %10)
+//     let num = (input % 10)*10 + (sum % 10)
+//     input = num;
+//     count ++
+//     if(result === num) break;
+// }
