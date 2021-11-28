92588515551e61930d8e8caa99c4ab949afb300a
sangkwonkim <sangkwon2406@naver.com>
2021-11-28 16:13:58 +0900

    sangkwon done

--- Toy Problem/33_LIS/sangkwon.js
+++ Toy Problem/33_LIS/sangkwon.js
@@ -11,23 +11,31 @@
 // subsequence: 'a', 'b', 'c', 'd', 'ab', 'ac', 'ad', 'bc', 'bd', 'cd', 'abc', 'abd', 'acd', 'bcd', 'abcd'
 // };
 const LIS = function (arr) {
-    const N = arr.length;
-    // lis[i]는 i에서 끝나는 LIS의 길이를 저장
-    // 최소한 각 요소 하나로 LIS를 만들 수 있으므로 1로 초기화한다.
-    const lis = Array(N).fill(1);
-    for (let i = 1; i < N; i++) {
-      // i에서 끝나는 LIS의 길이
-      for (let j = 0; j < i; j++) {
-        // i 이전의 인덱스만 검사하면 된다.
-        // i는 1부터 시작하므로, 짧은 길이부터 검사한다. (bottom-up 방식)
-        if (arr[i] > arr[j] && lis[i] < lis[j] + 1) {
-          lis[i] += 1;
-        }
+  const N = arr.length;
+  // lis[i]는 i에서 끝나는 LIS의 길이를 저장
+  // 최소한 각 요소 하나로 LIS를 만들 수 있으므로 1로 초기화한다.
+
+  // arr = [3, 10, 2, 1, 20]
+  const lis = Array(N).fill(1);
+  // lis = [1, 1, 1, 1, 1]
+
+  for (let i = 1; i < N; i++) {
+    // i에서 끝나는 LIS의 길이
+    for (let j = 0; j < i; j++) {
+      // i 이전의 인덱스만 검사하면 된다.
+      // i는 1부터 시작하므로, 짧은 길이부터 검사한다. (bottom-up 방식)
+      if (arr[i] > arr[j] && lis[i] < lis[j] + 1) {
+        lis[i] += 1;
+        // i가 1 ~ 4까지 가는 동안
+        // i = 1 => arr[i] = 10이다. 그러면 3보다 더 크기 때문에, LIS는 2개가 된다.
       }
     }
-    return Math.max(...lis);
-  };
-  // lis는 해당 위치까지의 배열로 잘랐을 때 만들어지는 lis를 나타낸다.
-  // 그래서 그 자리까지 위치하는 lis에다가 다음 요소가 왔을 때, lis를 충족할 수 있는 지 여부를 확인하고
-  // 거기에 1을 더해서 lis의 길이가 만들어지게 된다.
-  // 이렇게 이해하면 될 거 같다.
+  }
+  return Math.max(...lis);
+};
+// lis는 해당 위치까지의 배열로 잘랐을 때 만들어지는 lis를 나타낸다.
+// 그래서 그 자리까지 위치하는 lis에다가 다음 요소가 왔을 때, lis를 충족할 수 있는 지 여부를 확인하고(바로 앞의 요소보다 더 큰지)
+// 거기에 1을 더해서 lis의 길이가 만들어지게 된다.
+// 이렇게 이해하면 될 거 같다.
+  let output = LIS([2, 3]);
+console.log(output)
